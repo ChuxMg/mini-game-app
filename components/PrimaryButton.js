43f0c6f5024d363d875d9f834d81cbc0026906c3
Chuks Mgbojikwe <chuxmgbojikwe@gmail.com>
2024-04-27 13:00:43 +0700

    Mini-Game-App - initial commit - Improving The Buttons

---
+++ components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed!");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#5B8FB9" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 23,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#3581BE",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#D0E7DF",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
