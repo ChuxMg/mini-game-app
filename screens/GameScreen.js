a58cdf5210f9d279e3f92365f8d64f07955d2bef
Chuks Mgbojikwe <chuxmgbojikwe@gmail.com>
2024-04-28 23:34:12 +0700

    Mini-Game-App - Game Rounds Logs styled.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,7 @@ import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
 import Colors from "../constants/colors";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -69,6 +70,8 @@ function GameScreen({ userNumber, onGameOver }) {
     setGuessRounds((prevGuessRounds) => [newRandomNumber, ...prevGuessRounds]);
   }
 
+  const guessRoundsListLength = guessRounds.length;
+
   return (
     <View style={styles.screen}>
       <Title>Opponents</Title>
@@ -97,7 +100,12 @@ function GameScreen({ userNumber, onGameOver }) {
       {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
       <FlatList
         data={guessRounds}
-        renderItem={(itemData) => <Text>{itemData.item}</Text>}
+        renderItem={(itemData) => (
+          <GuessLogItem
+            roundNumber={guessRoundsListLength - itemData.index}
+            guess={itemData.item}
+          />
+        )}
         keyExtractor={(item) => item}
       />
     </View>
