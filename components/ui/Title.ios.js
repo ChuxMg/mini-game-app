397632997e9ccb2e3ddb0529fddc05d70aa304a4
Chuks Mgbojikwe <chuxmgbojikwe@gmail.com>
2024-05-03 22:58:07 +0700

    Mini-Game-App - Writing Platform-specific Code with the Platform API.

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
 import Colors from "../../constants/colors";
 
 function Title({ children }) {
@@ -14,8 +14,8 @@ const styles = StyleSheet.create({
     //fontWeight: "bold",
     color: Colors.asccent500,
     textAlign: "center",
-    borderWidth: 2,
-    borderColor: Colors.asccent500,
+    //borderWidth: Platform.OS === 'android' ? 2 : 0,
+    //borderWidth: Platform.select({ios: 0, android: 2}),
     padding: 12,
     maxWidth: "80%",
     width: 300,
